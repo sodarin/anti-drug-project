b7f6a3f93e2d14934dbce0e39bf12e82ec892fe8
qggcs <459684460@qq.com>
2019-09-30 13:29:50 +0800

    课程管理

--- src/app/course-management/file/file.component.ts
+++ src/app/course-management/file/file.component.ts
@@ -7,9 +7,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FileComponent implements OnInit {
 
+  listOfFiles: any[] = []
+
+  isAllDisplayDataChecked = false;
+  isOperating = false;
+  isIndeterminate = false;
+  listOfDisplayData: any[] = [];
+  listOfAllData: any[] = [];
+  mapOfCheckedId: { [key: string]: boolean } = {};
+  numberOfChecked = 0;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  currentPageDataChange($event: any[]): void {
+    this.listOfDisplayData = $event;
+    this.refreshStatus();
+  }
+
+  refreshStatus(): void {
+    this.isAllDisplayDataChecked = this.listOfDisplayData
+      .filter(item => !item.disabled)
+      .every(item => this.mapOfCheckedId[item.id]);
+    this.isIndeterminate =
+      this.listOfDisplayData.filter(item => !item.disabled).some(item => this.mapOfCheckedId[item.id]) &&
+      !this.isAllDisplayDataChecked;
+    this.numberOfChecked = this.listOfAllData.filter(item => this.mapOfCheckedId[item.id]).length;
+  }
+
+  checkAll(value: boolean): void {
+    this.listOfDisplayData.filter(item => !item.disabled).forEach(item => (this.mapOfCheckedId[item.id] = value));
+    this.refreshStatus();
   }
 
 }
--- src/app/course-management/question/question.component.ts
+++ src/app/course-management/question/question.component.ts
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuestionComponent implements OnInit {
 
+  listOfQuestionTypes: any[] = ["按题型", "单选题", "多选题", "问答题", "不定项选择题", "判断题", "填空题", "材料题"]
+  listOfCourses: any[] = ["按课程", "默认教学计划"]
+  result: any[] = ["2018-2019期中测试"];
+
+  count: number = 0;
+
+  selectedQuestionTypes: string = "按题型";
+  selectedCourse: string = "按课程";
+  selectVisible: boolean = false;
+
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  courseChange(value: string) {
+    if (this.selectedCourse != '按课程') {
+      this.selectVisible = true;
+    } else {
+      this.selectVisible = false;
+    }
   }
 
 }
