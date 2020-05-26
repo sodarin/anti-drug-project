1683f8222c290c9a3f546a90301d63d99551217f
Marshall-Sun <910204019@qq.com>
2020-05-26 20:50:32 +0800

    fix homepage

--- src/app/background/home-page/home-page.component.ts
+++ src/app/background/home-page/home-page.component.ts
@@ -7,13 +7,54 @@ import { NzMessageService } from 'ng-zorro-antd';
   styleUrls: ['./home-page.component.less']
 })
 export class HomePageComponent implements OnInit {
-  data = [
-    'Racing car sprays burning fuel into crowd.',
-    'Japanese princess to wed commoner.',
-    'Australian walks 100km after outback crash.',
-    'Man charged over missing wedding girl.',
-    'Los Angeles battles huge wildfires.'
+  notice = [
+    '公告1',
+    '公告2',
+    '公告3',
+    '公告4',
+    '公告5'
   ];
+
+  order = [
+    '订单1',
+    '订单2',
+    '订单3',
+    '订单4',
+    '订单5'
+  ];
+  
+  course = [
+    '课程1',
+    '课程2',
+    '课程3',
+    '课程4',
+    '课程5'
+  ];
+  
+  task = [
+    '任务1',
+    '任务2',
+    '任务3',
+    '任务4',
+    '任务5'
+  ];
+  
+  question = [
+    '问答1',
+    '问答2',
+    '问答3',
+    '问答4',
+    '问答5'
+  ];
+  
+  comment = [
+    '评论1',
+    '评论2',
+    '评论3',
+    '评论4',
+    '评论5'
+  ];
+  
 
   constructor(private msg: NzMessageService) {}
 
