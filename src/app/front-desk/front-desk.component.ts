9c3fd769e1239ecaa287f1ebe806611e9fc90db1
Marshall-Sun <910204019@qq.com>
2020-06-28 15:52:55 +0800

    merge frontdesk

--- src/app/front-desk/front-desk.component.ts
+++ src/app/front-desk/front-desk.component.ts
@@ -5,9 +5,9 @@ import { LoginModalComponent } from "../core/modal/login-modal/login-modal.compo
 import { RegisterModalComponent } from "../core/modal/register-modal/register-modal.component";
 
 @Component({
-  selector: 'app-front-desk',
-  templateUrl: './front-desk.component.html',
-  styleUrls: ['./front-desk.component.less'] 
+  selector: "app-front-desk",
+  templateUrl: "./front-desk.component.html",
+  styleUrls: ["./front-desk.component.less"],
 })
 export class FrontDeskComponent implements OnInit {
   isLogin: boolean = typeof window.localStorage.getItem("id") == "string";
@@ -41,7 +41,7 @@ export class FrontDeskComponent implements OnInit {
 
   login() {
     const modal = this._modalService.create({
-      nzTitle: '登录',
+      nzTitle: "登录",
       nzContent: LoginModalComponent,
       nzFooter: null,
       nzOnOk: () => {
@@ -52,10 +52,10 @@ export class FrontDeskComponent implements OnInit {
 
   register() {
     const modal = this._modalService.create({
-      nzTitle: '注册',
+      nzTitle: "注册",
       nzContent: RegisterModalComponent,
-      nzFooter: null
-    })
+      nzFooter: null,
+    });
   }
 
   logout() {
@@ -75,107 +75,4 @@ export class FrontDeskComponent implements OnInit {
       this.isCollapsed = true;
     }
   }
-
 }
-
-// @Component({
-//   selector: 'app-front-desk',
-//   templateUrl: './front-desk.component.html',
-//   styleUrls: ['./front-desk.component.less']
-// })
-// export class FrontDeskComponent implements OnInit {
-
-//   isLogin: boolean = true;//应用时设置成false
-//   isCollapsed: boolean = true;
-
-
-//   sixin = [];
-//   notificationData = [];
-
-//   userId: string = '1';
-
-//   constructor(
-//     private router: Router,
-//     private _modalService: NzModalService,
-//     private frontdeskServise: FrontDeskService,
-//     private privateChatService: PrivateChatService,
-//     private error: NzNotificationService,
-//     private msg: NzMessageService,
-//     private authService: AuthService
-//   ) { }
-
-//   serch(){
-//     this.frontdeskServise.getMessages('0' ,1,5,1).subscribe(result =>{
-//       this.sixin = result.data;
-//     },error1 => {
-//       this.error.create(
-//         'error',
-//         '发生错误',
-//         `${error1.error}`
-//       )
-//     })
-//     this.frontdeskServise.getNotifications('0',1, 5, 1).subscribe( result => {
-//       this.notificationData = result.data;
-//     }, error1 => {
-//       this.error.create(
-//         'error',
-//         '发生错误！',
-//         `${error1.error}`
-//       )
-//     })
-//   }
-//   ngOnInit() {
-//     this.privateChatService.changeStatus.subscribe(value => {
-//       this.serch();
-//     })
-//   }
-
-//   login() {
-//     const modal = this._modalService.create({
-//       nzTitle: '登录',
-//       nzContent: LoginModalComponent,
-//       nzFooter: null,
-//       nzMaskClosable:false,
-//       nzOnOk: () => this.isLogin = (typeof window.localStorage["id"] == "string")
-//     })
-//   }
-
-//   register() {
-//     const modal = this._modalService.create({
-//       nzTitle: '注册',
-//       nzContent: RegisterModalComponent,
-//       nzMaskClosable:false,
-//       nzFooter: null
-//     })
-//   }
-
-//   logout() {
-//     if (window.localStorage["id"]) {
-//       this.isLogin = false;
-//       window.localStorage.removeItem("id");
-//       window.localStorage.removeItem("password");
-//       window.localStorage.removeItem("salt");
-//       window.localStorage.removeItem("nickname");
-//       window.localStorage.removeItem("title");
-//       window.localStorage.removeItem("smallavatar");
-//       window.localStorage.removeItem("mediumavatar");
-//       window.localStorage.removeItem("largeavatar");
-//       window.localStorage.removeItem("roles");
-//       this.msg.success('注销成功');
-//     }
-//   }
-
-//   navigateByUrl(url: string) {
-//     this.router.navigateByUrl(url);
-//   }
-
-//   decodeUnicode(str) {
-//     str = str.replace(/\\/g, "%");
-//     str = unescape(str);
-//     var reg = new RegExp("%","g");
-//     str = str.replace(reg,"");
-//     return str;
-//   }
-
-
-// }
