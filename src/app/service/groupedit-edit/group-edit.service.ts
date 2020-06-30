e3c3f5b74fba5c2f3d5e1b03b7ab825e5ffa5067
Marshall-Sun <910204019@qq.com>
2020-06-30 23:05:01 +0800

    update groupthread and group-edit

--- src/app/service/groupedit-edit/group-edit.service.ts
+++ src/app/service/groupedit-edit/group-edit.service.ts
@@ -18,7 +18,20 @@ export class GroupEditService {
   }
   changeStatus: Subject<number> = new BehaviorSubject<number>(1);
   //话题中发表回复
-  publishReply(content:string,threadId:string){
+  getCommentListByArticleId(pageIndex: number, pageSize: number, articleId: string): Observable<any> {
+    return this._http.get(`/groupComment/groupsThreadComments?objId=${articleId}&pageNum=${pageIndex}&pageSize=${pageSize}`)
+  }
 
+  insertComment(comment: any, objecttype: string): Observable<any> {
+    return this._http.post(`/groupComment/insertComment`, {
+      content: comment.content,
+      objectid: comment.objectid,
+      userid: comment.userId,
+      objecttype: objecttype
+    })
+  }
+
+  deleteComment(id: string): Observable<any> {
+    return this._http.delete(`/groupComment/deleteComment?id=${id}`)
   }
 }
